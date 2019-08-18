a7ef287668fabb6f87fe9082983518bde8fd8cce
Paweł Jackowski <pjackowski@gmail.com>
2019-08-18 06:28:56 +0100

    Changed formatting, so words print better in default 80 char terminal.

--- passphrase-gen.sh
+++ passphrase-gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DICT=$(dirname "$0")/corncob-lowercase.txt
 for i in {1..10}; do
-  read -r -a words <<< $(shuf --random-source=/dev/urandom -n 5 $DICT | xargs)
-  echo ${words[*]} | awk '{ printf "%-16s %-16s %-16s %-16s %-16s\n", $1, $2, $3, $4, $5 }'
+  read -r -a words <<< $(shuf --random-source=/dev/urandom -n 4 $DICT | xargs)
+  echo ${words[*]} | awk '{ printf "%-18s %-18s %-18s %-18s\n", $1, $2, $3, $4 }'
 done
